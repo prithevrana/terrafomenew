22245482c68d247a18977df7e8d4657c2dcca72e
prithevrana <46280512+prithevrana@users.noreply.github.com>
2025-03-12 18:18:23 +0530

    Update variables.tf

--- tf-example/variables.tf
+++ tf-example/variables.tf
@@ -1,5 +1,3 @@
-variable "aws_region" {
-  description = "AWS region where resources will be deployed"
-  type        = string
-  default     = "us-east-2"
+provider "aws" {
+  region = var.aws_region
 }
